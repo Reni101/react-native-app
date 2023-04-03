a22a8ae96335fbbbf9315e3ec3170d72335bee2f
Maxim Dmitriev <Renissance@tut.by>
2023-04-03 11:15:08 +0300

    flex-box test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>new react native app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={[appStyles.nested1]}>
+
+            </View>
+            <View style={[appStyles.nested2]}>
+                <Text>Hello world!</Text>
+            </View>
+            <View style={[appStyles.nested3]}></View>
+            <View style={[appStyles.nested1]}></View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop:33,
+        backgroundColor:'white'
+    },
 });
+
+const appStyles = StyleSheet.create({
+    nested1: {
+        flex: 0.1,
+        backgroundColor: '#7a21ee',
+    },
+    nested2: {
+        flex: 0.3,
+        backgroundColor: 'white',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    nested3: {
+        flex: 1,
+        backgroundColor: 'red'
+    }
+})
