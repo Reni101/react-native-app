7af7cf3faf4bd3cb57f6260630f8a8b07217fb56
Maxim Dmitriev <Renissance@tut.by>
2023-04-05 12:15:33 +0300

    add styles

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import {FlatList, Image, ListRenderItem, StyleSheet, Text, View} from 'react-native';
 import {fakeData, ItemI} from "./src/data/FakeData";
+import {PADDING, WIDTH} from "./src/constant/constant";
 
 
 export default function App() {
     const renderItem: ListRenderItem<ItemI> = ({item}) => {
-        return <View>
-            <Image style={[styles.image]} source={item.image}/>
-            <Text>{item.title}</Text>
-            <Text>{item.price}</Text>
-
+        return <View style={[styles.itemProps]}>
+            <Image style={[styles.imageStyle]}
+                   resizeMode={'contain'}
+                   source={item.image}/>
+            <View style={[styles.infoIphone]}>
+                <Text>{item.title}</Text>
+                <Text>{item.price}</Text>
+            </View>
         </View>
     }
 
@@ -18,23 +22,36 @@ export default function App() {
                 data={fakeData}
                 renderItem={renderItem}
                 numColumns={2}
+                contentContainerStyle={{paddingHorizontal: PADDING}}
+                columnWrapperStyle={{justifyContent: 'space-between'}}
             />
-            <Image style={styles.image} source={require('./assets/favicon.png')}/>
-            <Image style={styles.image} source={require('./assets/phonePhoto/image1.png')}/>
-            {/*<Image style={styles.image} source={{uri:}}/>*/}
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: 'grey',
         flex: 1,
-        padding: 50,
-        alignItems: 'center'
     },
-    image: {
-        width: 50,
-        height: 50,
+    imageStyle: {
+        width: (WIDTH - PADDING * 2) / 2 - 8,
+        height: (WIDTH - PADDING * 2) / 2 - 8,
+    },
+    itemProps: {
+        backgroundColor: 'white',
+        width: (WIDTH - PADDING * 2) / 2 - 8,
+        borderWidth: 1,
+        borderColor: 'rgba(0,0,0,0.4)',
+        borderRadius: 5,
+        paddingTop: 10
+    },
+    infoIphone: {
+        marginTop: 19,
+        marginBottom: 22,
+        marginLeft: 12,
+        marginRight: 15
     }
 });
 
